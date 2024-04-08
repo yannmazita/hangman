43e6ab05a75bf65dbe6de129fe440816c2aacb73
Yann Mazita <dev.mazita@gmail.com>
2024-04-08 15:57:57 +0200

    Added continue screen modal

---
+++ front/src/components/GameContinueScreen.vue
@@ -0,0 +1,17 @@
+<template>
+    <button class="btn" onclick="my_modal_5.showModal()">open modal</button>
+    <dialog id="my_modal_5" class="modal modal-bottom">
+        <div class="modal-box">
+            <h3 class="font-bold text-lg">Hello!</h3>
+            <p class="py-4">Continue ?</p>
+            <div class="modal-action">
+                <form method="dialog">
+                    <button class="btn">Yes</button>
+                    <button class="btn">No</button>
+                </form>
+            </div>
+        </div>
+    </dialog>
+</template>
+<script setup lang="ts">
+</script
