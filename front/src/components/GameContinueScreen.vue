39eec3ed61569b20c118a7bec0865fd8fbfd171f
Yann Mazita <dev.mazita@gmail.com>
2024-04-10 11:37:59 +0200

    Updated game continue modal logic

--- front/src/components/GameContinueScreen.vue
+++ front/src/components/GameContinueScreen.vue
@@ -1,34 +1,65 @@
 <template>
     <dialog class="modal modal-bottom" :class="{ 'modal-open': showModal }">
         <div class="modal-box">
-            <h3 class="font-bold text-lg">{{ message }}!</h3>
+            <h3 class="font-bold text-lg">{{ message }}</h3>
             <p class="py-4">Continue ?</p>
             <div class="modal-action">
                 <form method="dialog">
-                    <button @click="continueChoice = true" class="btn">Yes</button>
-                    <button @click="continueChoice = false" class="btn">No</button>
+                    <button @click="continueGame(true)" class="btn">Yes</button>
+                    <button @click="continueGame(false)" class="btn">No</button>
                 </form>
             </div>
         </div>
     </dialog>
 </template>
 <script setup lang="ts">
-import { watch, ref, Ref } from 'vue';
+import { computed, watch, ref, Ref } from 'vue';
 import { storeToRefs } from 'pinia';
 import { useGameStore } from '@/stores/game.ts';
+import { useClientStore } from '@/stores/client.ts';
 
+const clientStore = useClientStore();
 const { game } = storeToRefs(useGameStore());
 const showModal = ref<boolean>(false);
-const continueChoice = ref<boolean>(false);
 const message: Ref<string> = ref("");
 
-watch(game, (newGame) => {
-    if (newGame.game_status !== 0) {
+const gameStatus = computed(() => {
+    return game.value.game_status;
+});
+
+const continueGame = async function (choice: boolean) {
+    if (choice) {
+        showModal.value = false;
+        try {
+            await clientStore.sendSocketMessage(JSON.stringify({
+                action: 'continue_game',
+                data: null
+            }));
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    else {
+        showModal.value = false;
+        try {
+            await clientStore.sendSocketMessage(JSON.stringify({
+                action: 'end_game',
+                data: null
+            }));
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+};
+
+watch(gameStatus, (newGameStatus) => {
+    if (newGameStatus !== 0) {
         showModal.value = true;
-        if (newGame.game_status === 1) {
+        if (newGameStatus === 1) {
             message.value = "You won!"
         }
-        else if (newGame.game_status === -1) {
+        else if (newGameStatus === -1) {
             message.value = "You lost!"
         }
     }
