00c68562697d45b39e7db60c982a158248f21785
Yann Mazita <dev.mazita@gmail.com>
2024-04-28 01:53:27 +0200

    Updated schemas

--- app/game/schemas.py
+++ app/game/schemas.py
@@ -4,3 +4,11 @@ from enum import Enum
 class GameAttribute(Enum):
     ID = "id"
     PLAYER_ID = "player_id"
+
+
+class GameStatsAttribute(Enum):
+    GUESSED_POSITIONS = "guessed_positions"
+    GUESSED_LETTERS = "guessed_letters"
+    TRIES_LEFT = "tries_left"
+    SUCCESSFUL_GUESSES = "successful_guesses"
+    GAME_STATUS = "game_status"
