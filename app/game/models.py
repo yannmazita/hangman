9e887f465b942f26e7222e881ca4c138b8ec66e9
Yann Mazita <dev.mazita@gmail.com>
2024-04-28 01:53:12 +0200

    Updated models

--- app/game/models.py
+++ app/game/models.py
@@ -4,26 +4,24 @@ from app.game.config import MAX_TRIES
 
 
 class GameBase(SQLModel):
-    player_id: UUID = Field(index=True, foreign_key="player.id", unique=True)
+    player_id: UUID | None = Field(default=None, index=True, foreign_key="player.id", unique=True)
 
 
 class Game(GameBase, table=True):
     id: UUID | None = Field(default=None, primary_key=True)
-    word_to_guess: str
-    guessed_positions: list[int]
-    guessed_letters: list[str]
+    word_to_guess: str = Field(default="")
+    word_progress: str = Field(default="")
+    guessed_positions: list[int] = Field(default=[])
+    guessed_letters: list[str] = Field(default=[])
     tries_left: int = Field(default=MAX_TRIES)
-    successful_guesses: int
-    game_status: int
-
-
-class GameCreate(GameBase):
-    player_id: UUID
+    successful_guesses: int = Field(default=0)
+    game_status: int = Field(default=0)
 
 
 class GameRead(GameBase):
     id: UUID
     word_to_guess: str
+    word_progress: str
     guessed_positions: list[int]
     guessed_letters: list[str]
     tries_left: int
