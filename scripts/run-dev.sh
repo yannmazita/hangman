48addb05d137396bb811f1132a6dfd72667b1b17
Yann Mazita <dev.mazita@gmail.com>
2024-07-14 17:24:53 +0200

    Fixed backend code reloads and improved backend dockerfile

--- scripts/run-dev.sh
+++ scripts/run-dev.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-# It either this or changing up the context in docker-compose to the parent directory
-cp poetry.lock app/poetry.lock
-cp pyproject.toml app/pyproject.toml
-
 docker compose --profile dev up "$@"
